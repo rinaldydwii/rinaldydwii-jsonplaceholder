06033b62a5e80c7e29d8ab85ff7b89ac21e0f7b8
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-09 19:09:31 +0700

    create container, navbar & footer components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,23 @@
+import Container from './Container'
 import Loading from './Loading'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import ProfileSection from './User/ProfileSection'
+import UsersSection from './lists/UsersSection'
+import PostsSection from './lists/PostsSection'
+import CommentsSection from './lists/CommentsSection'
+import AlbumsSection from './lists/AlbumsSection'
+import PhotosSection from './lists/PhotosSection'
 
 export {
-    Loading
+    Container,
+    Loading,
+    Navbar,
+    Footer,
+    ProfileSection,
+    UsersSection,
+    PostsSection,
+    CommentsSection,
+    AlbumsSection,
+    PhotosSection
 }
