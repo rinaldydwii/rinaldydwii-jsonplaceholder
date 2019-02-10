a5633f940555bcb4a748697c7ef9a5ab049d1154
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-10 18:28:10 +0700

    update read more button with button tag

--- src/components/ReadMoreButton.js
+++ src/components/ReadMoreButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
-const ReadMoreButton = ({to}) => (
+const ReadMoreButton = ({to, onClick}) => (
     <div className="text-center">
-        <Link to={to} className="button button__read-more">See more</Link>
+        { to ? 
+        ( <Link to={to} className="button button__read-more">See more</Link> ) : 
+        ( <button className="button button__read-more" onClick={onClick}>See more</button> ) }
     </div>
-)
+)        
+    
 export default ReadMoreButton;
