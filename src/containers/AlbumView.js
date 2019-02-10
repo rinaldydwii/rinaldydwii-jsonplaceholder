934359000ae22ae111e72cd6a0386e1d235a056c
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-10 11:35:29 +0700

    add loading in album view container

--- src/containers/AlbumView.js
+++ src/containers/AlbumView.js
@@ -1,39 +1,55 @@
 import React, { Component } from "react";
-import { Container, PhotosSection } from "../components";
+import { Container, PhotosSection, Loading } from "../components";
 
 class AlbumView extends Component {
     constructor() {
         super()
         this.state = {
             album: {},
-            loading: false,
-            finish: false,
-            error: ""
+            photos: [],
+            loadingAlbum: false,
+            finishAlbum: false,
+            errorAlbum: "",
+            loadingPhotos: false,
+            finishPhotos: false,
+            errorPhotos: ""
         }
     }
-    loadAlbum = async() => {
-        await fetch(`https://jsonplaceholder.typicode.com/albums/${this.props.match.params.id}`)
+    loadAlbum = () => {
+        this.setState({loadingAlbum: true})
+        fetch(`https://jsonplaceholder.typicode.com/albums/${this.props.match.params.id}`)
             .then(res => res.json())
-            .then(album => this.setState({album}))
-            .catch(error => this.setState({error, finish: true, loading: false}))
+            .then(album => this.setState({album, finishAlbum: true, loadingAlbum: false}))
+            .catch(errorAlbum => this.setState({errorAlbum, finishAlbum: true, loadingAlbum: false}))
     }
-    loadPhotos = async() => {
-        await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${this.props.match.params.id}`)
+    loadPhotos = () => {
+        this.setState({loadingPhotos: true})
+        fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${this.props.match.params.id}`)
             .then(res => res.json())
-            .then(photos => this.setState(prevState => ({album: {...prevState.album, photos}, finish: true, loading: false})))
-            .catch(error => this.setState({error, finish: true, loading: false}))
+            .then(photos => this.setState({photos, finishPhotos: true, loadingPhotos: false}))
+            .catch(errorPhotos => this.setState({errorPhotos, finishPhotos: true, loadingPhotos: false}))
     }
-    async componentDidMount() {
-        this.setState({loading: true})
-        await this.loadAlbum()
-        await this.loadPhotos()
+    componentDidMount() {
+        this.loadAlbum()
+        this.loadPhotos()
     }
     render() {
-        const { album, loading, finish, error } = this.state
+        const { album } = this.state
         return (
             <Container className="view">
-                <h1 className="text-center">{album.title}</h1>
-                <PhotosSection photos={album.photos} />
+                <Loading
+                    loading={this.state.loadingAlbum}
+                    finish={this.state.finishAlbum}
+                    error={this.state.errorAlbum}
+                >
+                    <h1 className="text-center">{album.title}</h1>
+                    <PhotosSection 
+                        photos={this.state.photos} 
+                        loading={this.state.loadingPhotos}
+                        finish={this.state.finishPhotos}
+                        error={this.state.errorPhotos}
+                    />
+                </Loading>
             </Container>
         );
     }
