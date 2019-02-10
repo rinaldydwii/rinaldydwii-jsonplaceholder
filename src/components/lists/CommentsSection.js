fd22cd51505c498c8060ae65214cf7c64095bcb4
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-10 11:30:08 +0700

    add loading in comments section component

--- src/components/lists/CommentsSection.js
+++ src/components/lists/CommentsSection.js
@@ -1,20 +1,29 @@
 import React from "react";
 import CommentItem from "../items/CommentItem";
+import CommentForm from "../comment/CommentForm";
+import Loading from "../Loading";
 import Container from "../Container";
 
-const CommentsSection = ({comments}) => (
+const CommentsSection = ({comments, loading, finish, error}) => (
     <section>
         <h2 className="text-center">Comments</h2>
-        { comments ? (
-            <div className="list">
-                <Container small>
-                    { comments.map(comment => (
-                            <CommentItem comment={comment} />
-                        ))
-                    }
-                </Container>
-            </div>
-        ) : <div>Comments not found!</div>}
+        <Loading
+            loading={loading}
+            finish={finish}
+            error={error}
+        >
+            <Container small>
+                { comments ? (
+                    <div className="list">
+                        { comments.map(comment => (
+                                <CommentItem comment={comment} key={comment.id} />
+                            ))
+                        }
+                    </div>
+                ) : <div>Comments not found!</div>}
+                <CommentForm />
+            </Container>
+        </Loading>
     </section>
 )
 export default CommentsSection;
