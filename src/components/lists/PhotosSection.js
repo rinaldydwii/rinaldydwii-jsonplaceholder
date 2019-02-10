b8cc4e46601e5251567250a2ffcf10e612fb42fd
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-10 18:49:04 +0700

    update pagination for photos data

--- src/components/lists/PhotosSection.js
+++ src/components/lists/PhotosSection.js
@@ -3,20 +3,20 @@ import PhotoItem from "../items/PhotoItem";
 import Loading from "../Loading";
 import ReadMoreButton from "../ReadMoreButton";
 
-const PhotosSection = ({photos, loading, finish, error, paginateToPage = false}) => (
+const PhotosSection = ({photos, loading, finish, error, onLoadPhotos, paginateToPage = false, paginate = false}) => (
     <section>
         <h2 className="text-center">Photos</h2>
         <Loading loading={loading} finish={finish} error={error}>
             { photos ? (
                 <div className="grid grid-4">
                     { photos.map(photo => (
-                            <PhotoItem photo={photo} key={photo.id} />
-                        ))
-                    }
+                        <PhotoItem photo={photo} key={photo.id} />
+                    )) }
                 </div>
             ) : <div>Photos not found!</div>
             }
-            { paginateToPage ? <ReadMoreButton to="/users" /> : "" }
+            { paginateToPage ? <ReadMoreButton to="/photos" /> : "" }
+            { paginate ? <ReadMoreButton onClick={onLoadPhotos} /> : ""}
         </Loading>
     </section>
 )
--- src/containers/PhotosView.js
+++ src/containers/PhotosView.js
@@ -8,17 +8,23 @@ class PhotosView extends Component {
             photos: [],
             loading: false,
             finish: false,
-            error: ""
+            error: "",
+            page: 1
         }
     }
     loadPhotos = () => {
-        fetch("https://jsonplaceholder.typicode.com/photos")
+        const { page } = this.state
+        const limit = 12
+        this.setState({loading: page === 1 ? true : false})
+        fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
             .then(res => res.json())
-            .then(photos => this.setState({photos, finish: true, loading: false}))
-            .catch(error => this.setState({error, finish: true}))
+            .then(photos => {
+                if (!photos.length || photos.length < limit) return this.setState({page: null})
+                this.setState(prevState => ({photos: [...prevState.photos, ...photos], finish: true, loading: false, page: page + 1}))
+            })
+            .catch(error => this.setState({error, finish: true, loading: false}))
     }
     componentDidMount() {
-        this.setState({loading: true})
         this.loadPhotos()
     }
     render() {
@@ -29,6 +35,8 @@ class PhotosView extends Component {
                     loading={this.state.loading}
                     finish={this.state.finish}
                     error={this.state.error}
+                    onLoadPhotos={this.loadPhotos}
+                    paginate={this.state.page ? true : false}
                 />
             </Container>
         );
