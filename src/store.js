ce92a5070299858a12a473c81b46ce5cceadb69d
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-11 14:58:19 +0700

    create store

--- src/store.js
+++ src/store.js
@@ -2,10 +2,21 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 
-import userReducers from './reducers/userReducers'
+import {userReducer, usersReducer} from './reducers/userReducers'
+import postsReducer from './reducers/postReducers'
+import commentsReducer from './reducers/commentReducers'
+import albumsReducer from './reducers/albumReducers'
+import photosReducer from './reducers/photoReducers'
 
 const loggerMiddleware = createLogger();
-const reducers = combineReducers({userReducers});
+const reducers = combineReducers({
+    userReducer, 
+    usersReducer,
+    postsReducer,
+    commentsReducer,
+    albumsReducer,
+    photosReducer
+});
 
 export default function configureStore() {
     return createStore(
