121aeca93a73a61f1228d0afd81a832decf57b7b
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-11 23:22:47 +0800

    update redux home view

--- src/containers/HomeView.js
+++ src/containers/HomeView.js
@@ -1,92 +1,78 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import { UsersSection, PostsSection, AlbumsSection, PhotosSection, Container } from "../components";
+import { fetchPosts } from "../actions/postActions";
+import { fetchUsers } from "../actions/userActions";
+import { fetchAlbums } from "../actions/albumActions";
+import { fetchPhotos } from "../actions/photoActions";
 
 class HomeView extends Component {
-    constructor() {
-        super()
-        this.state = {
-            users: [],
-            loadingUsers: false,
-            finishUsers: false,
-            errorUsers: "",
-            posts: [],
-            loadingPosts: false,
-            finishPosts: false,
-            errorPosts: "",
-            albums: [],
-            loadingAlbums: false,
-            finishAlbums: false,
-            errorAlbums: "",
-            photos: [],
-            loadingPhotos: false,
-            finishPhotos: false,
-            errorPhotos: ""
-        }
-    }
-    loadUsers = () => {
-        fetch("https://jsonplaceholder.typicode.com/users?_limit=8")
-            .then(res => res.json())
-            .then(users => this.setState({users, finishUsers: true, loadingUsers: false}))
-            .catch(errorUsers => this.setState({errorUsers, finishUsers: true, loadingUsers: false}))
-    }
-    loadPosts = () => {
-        fetch("https://jsonplaceholder.typicode.com/posts?_limit=8")
-            .then(res => res.json())
-            .then(posts => this.setState({posts, finishPosts: true, loadingPosts: false}))
-            .catch(errorPosts => this.setState({errorPosts, finishPosts: true, loadingPosts: false}))
-    }
-    loadAlbums = () => {
-        fetch("https://jsonplaceholder.typicode.com/albums?_limit=8")
-            .then(res => res.json())
-            .then(albums => this.setState({albums, finishAlbums: true, loadingAlbums: false}))
-            .catch(errorAlbums => this.setState({errorAlbums, finishAlbums: true, loadingAlbums: false}))
-    }
-    loadPhotos = () => {
-        fetch("https://jsonplaceholder.typicode.com/photos?_limit=8")
-            .then(res => res.json())
-            .then(photos => this.setState({photos, finishPhotos: true, loadingPhotos: false}))
-            .catch(errorPhotos => this.setState({errorPhotos, finishPhotos: true, loadingPhotos: false}))
-    }
     componentDidMount() {
-        this.setState({loadingUsers: true, loadingPosts: true, loadingAlbums: true, loadingPhotos: true})
-        this.loadUsers()
-        this.loadPosts()
-        this.loadAlbums()
-        this.loadPhotos()
+        this.props.getUsers()
+        this.props.getPosts()
+        this.props.getAlbums()
+        this.props.getPhotos()
+        
     }
     render() {
         return (
             <Container>
                 <UsersSection 
-                    users={this.state.users} 
-                    loading={this.state.loadingUsers}
-                    finish={this.state.finishUsers}
-                    error={this.state.errorUsers}
-                    paginateToPage
+                    users={this.props.users} 
+                    loading={this.props.loadingUsers}
+                    finish={this.props.finishUsers}
+                    error={this.props.errorUsers}
+                    // paginateToPage
                 />
                 <PostsSection 
-                    posts={this.state.posts} 
-                    loading={this.state.loadingPosts}
-                    finish={this.state.finishPosts}
-                    error={this.state.errorPosts}
-                    paginateToPage
+                    posts={this.props.posts} 
+                    loading={this.props.loadingPosts}
+                    finish={this.props.finishPosts}
+                    error={this.props.errorPosts}
+                    // paginateToPage
                 />
                 <AlbumsSection 
-                    albums={this.state.albums} 
-                    loading={this.state.loadingAlbums}
-                    finish={this.state.finishAlbums}
-                    error={this.state.errorAlbums}
-                    paginateToPage
+                    albums={this.props.albums} 
+                    loading={this.props.loadingAlbums}
+                    finish={this.props.finishAlbums}
+                    error={this.props.errorAlbums}
+                    // paginateToPage
                 />
                 <PhotosSection 
-                    photos={this.state.photos} 
-                    loading={this.state.loadingPhotos}
-                    finish={this.state.finishPhotos}
-                    error={this.state.errorPhotos}
-                    paginateToPage
+                    photos={this.props.photos} 
+                    loading={this.props.loadingPhotos}
+                    finish={this.props.finishPhotos}
+                    error={this.props.errorPhotos}
+                    // paginateToPage
                 />
             </Container>
         );
     }
 }
-export default HomeView;
+
+const mapStateToProps = state => ({
+    users: state.usersReducer.users,
+    loadingUsers: state.usersReducer.loading,
+    finishUsers: state.usersReducer.finish,
+    errorUsers: state.usersReducer.error,
+    posts: state.postsReducer.posts,
+    loadingPosts: state.postsReducer.loading,
+    finishPosts: state.postsReducer.finish,
+    errorPosts: state.postsReducer.error,
+    loadingAlbums: state.albumsReducer.loading,
+    finishAlbums: state.albumsReducer.finish,
+    errorAlbums: state.albumsReducer.error,
+    photos: state.photosReducer.photos,
+    loadingPhotos: state.photosReducer.loading,
+    finishPhotos: state.photosReducer.finish,
+    errorPhotos: state.photosReducer.error,
+    albums: state.albumsReducer.albums,
+})
+  
+const mapDispatchToProps = (dispatch) => ({
+    getUsers: () => dispatch(fetchUsers()),
+    getPosts: () => dispatch(fetchPosts()),
+    getAlbums: () => dispatch(fetchAlbums()),
+    getPhotos: () => dispatch(fetchPhotos())
+})
+export default connect(mapStateToProps, mapDispatchToProps)(HomeView);
