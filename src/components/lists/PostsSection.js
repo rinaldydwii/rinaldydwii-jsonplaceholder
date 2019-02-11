59d8a00e98840564dc295ad3eebdb07b79093c0b
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-12 00:25:04 +0800

    create add button post on posts section

--- src/components/lists/PostsSection.js
+++ src/components/lists/PostsSection.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import PostItem from "../items/PostItem";
 import Loading from "../Loading";
 import ReadMoreButton from "../ReadMoreButton";
 
-const PostsSection = ({posts, loading, finish, error, onLoadPosts, paginateToPage = false, paginate = false}) => (
+const PostsSection = ({posts, loading, finish, error, onLoadPosts, paginateToPage = false, paginate = false, addButton = false}) => (
     <section>
         <h2 className="text-center">Posts</h2>
+        { addButton ? 
+            (
+                <div className="text-right" style={{marginBottom: 20}}>
+                    <Link className="button" to="/posts/add">Add</Link>
+                </div>
+            ) : ""
+        }
         <Loading loading={loading} finish={finish} error={error}>
             { posts ? (
                 <div className="grid grid-4">
