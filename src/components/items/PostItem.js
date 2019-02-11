b256ea0525a12a27857635ab72fd5b06c2213349
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-11 23:20:04 +0800

    delete action in post item

--- src/components/items/PostItem.js
+++ src/components/items/PostItem.js
@@ -5,10 +5,6 @@ const PostItem = ({post}) => (
     <Link to={`/posts/${post.id}`}>
         <div className="grid__item">
             <div className="grid__title">{post.title}</div>
-            <div className="text-right">
-                <button className="button button__action">Edit</button>
-                <button className="button button__action">Delete</button>
-            </div>
         </div>
     </Link>
 )
