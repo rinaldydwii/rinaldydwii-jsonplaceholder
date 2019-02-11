6b6984267ec5981083908cb461eb06e0529d5647
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-11 08:38:58 +0700

    config api url

---
+++ src/config/apiUrl.js
@@ -0,0 +1,15 @@
+export const MAIN_API_URL = "https://jsonplaceholder.typicode.com/";
+
+const USER_ROUTE = "users"
+const POSTS_ROUTE = "posts"
+const COMMENTS_ROUTE = "comments"
+const ALBUMS_ROUTE = "albums"
+const PHOTOS_ROUTE = "photos"
+
+export const apiUrl = {
+    users: MAIN_API_URL+USER_ROUTE,
+    posts: MAIN_API_URL+POSTS_ROUTE,
+    comments: MAIN_API_URL+COMMENTS_ROUTE,
+    albums: MAIN_API_URL+ALBUMS_ROUTE,
+    photos: MAIN_API_URL+PHOTOS_ROUTE,
+}
