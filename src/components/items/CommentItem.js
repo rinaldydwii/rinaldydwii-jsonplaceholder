21093f16143e5127fa2d1eda9b77062c2fd00b12
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-11 08:37:46 +0700

    add action on comment item

--- src/components/items/CommentItem.js
+++ src/components/items/CommentItem.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const CommentItem = ({comment}) => (
+const CommentItem = ({comment, onDeleteComment}) => (
     <div className="list__item">
         <div className="list__title">{comment.name}</div>
         <div className="list__subtitle">{comment.email}</div>
         <div className="list__content"><p>{comment.body}</p></div>
+        <div className="text-right">
+            <button className="button button__action">Edit</button>
+            <button className="button button__action" onClick={onDeleteComment}>Delete</button>
+        </div>
     </div>
 )
 export default CommentItem;
